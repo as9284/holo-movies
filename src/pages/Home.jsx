26628f6c9b7727f44db6b25c61969b3df8a2756f
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-03 15:24:31 +0200

    Add the inter font globally

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <div>
-        <h1>Home</h1>
+      <div className="max-w-7xl min-h-screen m-auto p-4 flex flex-col items-center justify-center">
+        <h1>Holo Movies</h1>
       </div>
     </>
   );
