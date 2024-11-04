bc252bc8426bbc0e369c4d091713eb15060ba358
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-04 12:20:13 +0200

    Improve hover animation and movie results design

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -27,15 +27,15 @@ export const MovieList = ({
                   alt={movie.Title}
                   className="absolute inset-0 w-full h-full object-cover"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-end p-4 text-center group-hover:backdrop-blur-xl group-hover:bg-opacity-70 duration-200">
-                  <h2 className="text-lg font-semibold group-hover:text-2xl group-hover:-translate-y-36 duration-200">
-                    {movie.Title}
-                  </h2>
-                  <p className="text-sm text-neutral-400 group-hover:-translate-y-36 duration-200">
-                    {movie.Year.endsWith("–")
-                      ? movie.Year.slice(0, -1)
-                      : movie.Year}
-                  </p>
+                <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center p-4 text-center group-hover:backdrop-blur-xl group-hover:bg-opacity-70 duration-200">
+                  <div className="w-full h-full flex flex-col justify-center items-center">
+                    <h2 className="text-lg font-semibold">{movie.Title}</h2>
+                    <p className="text-sm text-neutral-400 font-semibold">
+                      {movie.Year.endsWith("–")
+                        ? movie.Year.slice(0, -1)
+                        : movie.Year}
+                    </p>
+                  </div>
                 </div>
               </div>
             ))}
